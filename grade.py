b7a400ba455eb84211942f1ca4c25fc6d539c44d
navaneeth <nvnthsheri14@gmail.com>
2026-06-24 12:56:53 +0530

    Add grading system based on marks input

---
+++ grade.py
@@ -0,0 +1,11 @@
+marks = int(input("Enter marks:"))
+
+if marks >= 90:
+    print("Excellent")
+elif marks >=75:
+    print("very good")
+elif marks >= 35:
+    print("pass")
+else:
+    print("fail")
+    
